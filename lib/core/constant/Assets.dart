c8c73998a9570ee1c03779ea3f5ae48706a2f859
Mera Refat <merarefat@gmail.com>
2026-03-25 21:24:58 +0200

    add: create Assets and Icons constants for image paths

---
+++ lib/core/constant/Assets.dart
@@ -0,0 +1,25 @@
+class Assets {
+  // Avatars
+  static const String avatar1 = 'Assets/images/avatar1.png';
+  static const String avatar2 = 'Assets/images/avatar2.png';
+  static const String avatar3 = 'Assets/images/avatar3.png';
+  static const String avatar4 = 'Assets/images/avatar4.png';
+  static const String avatar5 = 'Assets/images/avatar5.png';
+  static const String avatar6 = 'Assets/images/avatar6.png';
+  static const String avatar7 = 'Assets/images/avatar7.png';
+  static const String avatar8 = 'Assets/images/avatar8.png';
+  static const String avatar9 = 'Assets/images/avatar9.png';
+
+  // Onboarding images
+  static const String onboarding1 = 'Assets/images/onboarding1.png';
+  static const String onboarding2 = 'Assets/images/onboarding2.png';
+  static const String onboarding3 = 'Assets/images/onboarding3.png';
+  static const String onboarding4 = 'Assets/images/onboarding4.png';
+  static const String onboarding5 = 'Assets/images/onboarding5.png';
+  static const String onboarding11 = 'Assets/images/onboarding11.png';
+
+  // Icons / Logos
+  static const String playIcon = 'Assets/images/play_icon.png';
+  static const String routeLogo = 'Assets/images/route_logo.png';
+  static const String supervisedBy = 'Assets/images/supervised_by.png';
+}
