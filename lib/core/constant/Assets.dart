3c16e50a47d371d42e83b47a625d6f8e77ef8f03
3bowhab <ali.abdelwhab21@gmail.com>
2026-03-26 22:28:57 +0200

    edit asset

--- lib/core/constant/Assets.dart
+++ lib/core/constant/Assets.dart
@@ -1,25 +1,25 @@
 class Assets {
   // Avatars
-  static const String avatar1 = 'Assets/images/avatar1.png';
-  static const String avatar2 = 'Assets/images/avatar2.png';
-  static const String avatar3 = 'Assets/images/avatar3.png';
-  static const String avatar4 = 'Assets/images/avatar4.png';
-  static const String avatar5 = 'Assets/images/avatar5.png';
-  static const String avatar6 = 'Assets/images/avatar6.png';
-  static const String avatar7 = 'Assets/images/avatar7.png';
-  static const String avatar8 = 'Assets/images/avatar8.png';
-  static const String avatar9 = 'Assets/images/avatar9.png';
+  static const String avatar1 = 'assets/images/avatar1.png';
+  static const String avatar2 = 'assets/images/avatar2.png';
+  static const String avatar3 = 'assets/images/avatar3.png';
+  static const String avatar4 = 'assets/images/avatar4.png';
+  static const String avatar5 = 'assets/images/avatar5.png';
+  static const String avatar6 = 'assets/images/avatar6.png';
+  static const String avatar7 = 'assets/images/avatar7.png';
+  static const String avatar8 = 'assets/images/avatar8.png';
+  static const String avatar9 = 'assets/images/avatar9.png';
 
   // Onboarding images
-  static const String onboarding1 = 'Assets/images/onboarding1.png';
-  static const String onboarding2 = 'Assets/images/onboarding2.png';
-  static const String onboarding3 = 'Assets/images/onboarding3.png';
-  static const String onboarding4 = 'Assets/images/onboarding4.png';
-  static const String onboarding5 = 'Assets/images/onboarding5.png';
-  static const String onboarding11 = 'Assets/images/onboarding11.png';
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+  static const String onboarding4 = 'assets/images/onboarding4.png';
+  static const String onboarding5 = 'assets/images/onboarding5.png';
+  static const String onboarding11 = 'assets/images/onboarding11.png';
 
   // Icons / Logos
-  static const String playIcon = 'Assets/images/play_icon.png';
-  static const String routeLogo = 'Assets/images/route_logo.png';
-  static const String supervisedBy = 'Assets/images/supervised_by.png';
+  static const String playIcon = 'assets/images/play_icon.png';
+  static const String routeLogo = 'assets/images/route_logo.png';
+  static const String supervisedBy = 'assets/images/supervised_by.png';
 }
